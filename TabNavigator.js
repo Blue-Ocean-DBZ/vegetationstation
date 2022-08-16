ea39361c7dbc8b470ff8f819cac880f54e5d288f
Kevin Ha <kevincw.ha@gmail.com>
2022-08-16 16:56:42 -0700

    navigation updated to include useContext to pass props

--- TabNavigator.js
+++ TabNavigator.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from './zdc/Home.js';
@@ -13,23 +14,26 @@ import EditProfile from './zdc/EditProfile.js';
 import PlantPage from './zdc/PlantPage.js';
 import TradeRequests from './zdc/TradeRequests.js';
 
-//multiple stack navigations inside individual tab navigations
+import { auth } from './firebase.js';
+
+//Navigators
 
 const Tab = createBottomTabNavigator();
-
 const HomeStack = createNativeStackNavigator();
 const MyPlantStack = createNativeStackNavigator();
 const TradeStack = createNativeStackNavigator();
 const FavoritesStack = createNativeStackNavigator();
 
-function HomeStackScreen() {
+// Stack Navigations for each individual tabs
+
+function HomeStackScreen(props) {
   return (
     <HomeStack.Navigator>
       <HomeStack.Screen name="Home" component={Home} />
       <HomeStack.Screen name="Profile" component={Profile} />
       <HomeStack.Screen name="Edit Profile" component={EditProfile} />
       <HomeStack.Screen name="Plant Card" component={PlantPage} />
-      <HomeStack.Screen name="Trade Requests" component={TradeRequests} />
+      <HomeStack.Screen name="Trade Modal" component={TradeRequests} />
     </HomeStack.Navigator>
   );
 }
@@ -38,6 +42,7 @@ function MyPlantStackScreen() {
   return (
     <MyPlantStack.Navigator>
       <MyPlantStack.Screen name="My Plants" component={MyPlants} />
+      {/* <MyPlantStack.Screen name="Profile" component={Profile} /> */}
     </MyPlantStack.Navigator>
   );
 }
@@ -58,6 +63,94 @@ function FavoritesStackScreen() {
   );
 }
 
+// Use Context for passing props
+export const PlantContext = React.createContext()
+
+export function usePlant () {
+  const {test1 , test2, test3} = useContext(PlantContext);
+  return {test1, test2, test3};
+}
+
+// Tab Navigator, individual stack navigators are nested inside
+
+export default function TabNavigator() {
+
+  console.log(auth.currentUser.uid);
+
+  const [messages, setMessages] = useState(null);
+  const [string, setString] = useState('This is working');
+  const [plantArray, setPlantArray] = useState([1, 2, 3]);
+
+  return (
+    <PlantContext.Provider
+      value ={{
+        test1: [messages, setMessages],
+        test2: [string, setString],
+        test3: [plantArray, setPlantArray]}}>
+      <Tab.Navigator screenOptions={{
+        tabBarStyle: {backgroundColor: "#8eb69b"},
+        tabBarActiveTintColor: "#fefae0",
+        tabBarInactiveTintColor: "black",
+        // tabBarShowLabel: false,
+        headerShown: false,
+      }}>
+        <Tab.Screen
+          name="HomePage" component={HomeStackScreen}
+          options={({ route }) => ({
+            tabBarStyle: {
+              display: getTabBarVisibility(route),
+              backgroundColor: "#8eb69b"
+            },
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="home-outline" color={color} size={size}/>
+            ),
+          })}/>
+        <Tab.Screen
+          name="MyPlants" component={MyPlantStackScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="leaf-outline" color={color} size={size}/>
+            )
+          }}/>
+        <Tab.Screen
+          name="Trades" component={TradeStackScreen}
+          options={({ route }) => ({
+            tabBarStyle: {
+              display: getTabBarVisibility(route),
+              backgroundColor: "#8eb69b"
+            },
+            tabBarBadge: messages,
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="chatbox-ellipses-outline" color={color} size={size}/>
+            ),
+          })}/>
+        <Tab.Screen
+          name="Favorites" component={FavoritesStackScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <Ionicons name="heart-outline" color={color} size={size}/>
+            )
+          }}/>
+      </Tab.Navigator>
+    </PlantContext.Provider>
+  );
+}
+
+// Helper function for removing Tab Bar visibility
+
+const getTabBarVisibility = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? '';
+
+  if (routeName == 'Profile') {
+    return 'none';
+  }
+  if (routeName == 'Trade') {
+    return 'none';
+  }
+  return 'flex';
+}
+
+
 // #b3cb9b
 // #bc6c25
 // #283618
@@ -68,45 +161,10 @@ function FavoritesStackScreen() {
 // #2c3d36
 // #b2b2b2
 
-export default function TabNavigator() {
+//8eb69b
+//daf1de
+//606c38
 
-  return (
-      <Tab.Navigator screenOptions={{
-        tabBarStyle: {backgroundColor: "#606c38"},
-        tabBarActiveTintColor: "#fefae0",
-        tabBarInactiveTintColor: "black",
-        // tabBarShowLabel: false,
-        headerShown: false,
-      }}>
-        <Tab.Screen
-          name="HomePage" component={HomeStackScreen}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Ionicons name="home-outline" color={color} size={size}/>
-            )
-          }}/>
-        <Tab.Screen
-          name="MyPlants" component={MyPlantStackScreen}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Ionicons name="leaf-outline" color={color} size={size}/>
-            )
-          }}/>
-        <Tab.Screen
-          name="Trades" component={TradeStackScreen}
-          options={{
-            tabBarBadge: 3,
-            tabBarIcon: ({color, size}) => (
-              <Ionicons name="chatbox-ellipses-outline" color={color} size={size}/>
-            )
-          }}/>
-        <Tab.Screen
-          name="Favorites" component={FavoritesStackScreen}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Ionicons name="heart-outline" color={color} size={size}/>
-            )
-          }}/>
-      </Tab.Navigator>
-  );
-}
+
+//907a48
+//fefae0
--- zdc/Home.js
+++ zdc/Home.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { usePlant } from '../TabNavigator.js'
 
 export default function Home({ navigation }) {
 
+  const {test1 , test2} = usePlant();
+  const [messages, setMessages] = test1;
+  const [string, setString] = test2;
+  // const [plantArray, setPlantArray] = test3;
+
   return (
-    <View style={styles.container}>
-      <Text>Home Page</Text>
-      <Button
-        title="Profile"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
+      <View style={styles.container}>
+        <Text>{string}</Text>
+        <Button
+          title="Profile"
+          onPress={() => navigation.navigate('Profile')}
+        />
+        <Button
+          title="Trade Requests"
+          onPress={() => navigation.navigate('Trade Requests')}
+        />
+        <Button
+          title="Set the Messages"
+          onPress={() => setMessages(5)}
+        />
+      </View>
   );
 }
 
