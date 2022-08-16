3ea1b58a4b35be5c9c83eb031a8540c2502df589
Jonathan Oh <jonoh832@gmail.com>
2022-08-15 17:11:57 -0700

    Completed filtering

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -6,17 +6,22 @@ import * as ImagePicker from 'expo-image-picker';
 const Home = (props) => {
 
   const [searchTerm, setSearchTerm] = useState('');
-  const [plantsList, setPlantsList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [dummyData, setDummyData] = useState([]);
 
   const onChangeSearch = query => {
     setSearchTerm(query);
     let filteredListing = [];
-
-    if (query.length > 0) {
-      const regex = new RegExp(`^${value}`, 'i');
-      filteredListing = plantsList.sort().filter(v => regex.test(v.name))
-      setPlantsList(filteredListing);
+    console.log('query', query);
+    if (query) {
+      const regex = new RegExp(`^${query}`, 'i');
+      filteredListing = DATA.filter(v => regex.test(v.name))
+      console.log('DATA FILTER',filteredList)
+      setFilteredList(filteredListing);
+    } else {
+      setFilteredList([]);
     }
+
   }
 
   const renderSuggestions = () => {
@@ -24,18 +29,6 @@ const Home = (props) => {
       return null
     } else {
       //            data={suggestions}
-        const renderCard = ({plant}) => (
-          <Card>
-            <View style= {styles.plantCardContainer}>
-              <Card.Cover style= {styles.plantImage} source= {{url: plant.img}} />
-              <View style= {styles.plantCardInformation}>
-                <Title style= {styles.plantName}>{plant.name}</Title>
-                <Paragraph>{plant.location} (XXXX mi away)</Paragraph>
-                <Paragraph>{plant.username}</Paragraph>
-              </View>
-            </View>
-          </Card>
-        )
         return (
           <FlatList>
             data={fakeData}
@@ -47,38 +40,6 @@ const Home = (props) => {
 
   }
 
-  const DATA = [
-    {
-    name: "Plantlord",
-    img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-    username: 'Test',
-    location: 'Denmark'
-  },
-  {
-    name: "Plantlord2",
-    img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-    username: 'Test',
-    location: 'Denmark'
-  },
-  {
-    name: "Plantlord3",
-    img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-    username: 'Test',
-    location: 'Denmark'
-  },
-  {
-    name: "Plantlord4",
-    img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-    username: 'Test',
-    location: 'Denmark'
-  },
-  {
-    name: "Plantlord5",
-    img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
-    username: 'Test',
-    location: 'Denmark'
-  }
-]
 
 const renderCard = ({item}) => (
   <Card>
@@ -100,10 +61,10 @@ const renderCard = ({item}) => (
           <Title style= {styles.headerTitle}>Listings</Title>
         </View>
         <SafeAreaView>
-            <FlatList
-              data={DATA}
-              renderItem={renderCard}
-            />
+          {filteredList.length > 0 ? <FlatList data={filteredList} renderItem={renderCard}/>
+          : searchTerm ? <FlatList data={filteredList} renderItem={renderCard}/>
+          : <FlatList data={DATA} renderItem={renderCard}/>}
+
         </SafeAreaView>
       </View>
     </View>
@@ -111,6 +72,39 @@ const renderCard = ({item}) => (
 }
 
 export default Home;
+
+const DATA = [
+  {
+  name: "Plantlord",
+  img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
+  username: 'Test',
+  location: 'Denmark'
+},
+{
+  name: "Plantlord2",
+  img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
+  username: 'Test',
+  location: 'Denmark'
+},
+{
+  name: "Plantlord3",
+  img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
+  username: 'Test',
+  location: 'Denmark'
+},
+{
+  name: "Plantlord4",
+  img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
+  username: 'Test',
+  location: 'Denmark'
+},
+{
+  name: "A apple",
+  img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/indoor-plants-1634736990.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*',
+  username: 'Test',
+  location: 'Denmark'
+}
+]
 
 const styles= StyleSheet.create({
   overallContainer: {
